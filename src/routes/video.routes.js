a49451589b5e317d9d3419a50189651f6611cad3
Mohit Chouhan <info.mohit321@gmail.com>
2025-05-14 21:57:11 +0100

    Create video route functionality and video router.

---
+++ src/routes/video.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import { getAllVideos, publishAVideo, getVideoById, updateVideo, deleteVideo, togglePublishStatus } from "../controllers/video.controller.js";
+
+import { verifyJWT } from "../middleware/auth.middlewares.js";
+
+import { upload } from "../middleware/multer.middlewares.js";
+
+const router = Router();
+
+router.route("/videos").get(verifyJWT, getAllVideos);
+router.route("/uploadVideo").post(verifyJWT, upload.fields([
+    {
+        name: "thumbnailFile",
+        maxCount: 1
+    },
+    {
+        name: "videoFile",
+        maxCount: 1
+    }
+]), publishAVideo)
+
+export default router;
