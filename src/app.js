43a9dd6abfc5b17da70407f9db49f1f4908fb44c
Mohit Chouhan <info.mohit321@gmail.com>
2025-05-14 21:51:23 +0100

    Add video router.

--- src/app.js
+++ src/app.js
@@ -17,10 +17,12 @@ app.use(cookieParser())
 
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 import { errorHandler } from "./middleware/error.middlewares.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/video", videoRouter);
 app.use(errorHandler)
 
 export { app };
