b082b5a3f66f3739ba1bc3c123beb33167783a84
Mohit Chouhan <info.mohit321@gmail.com>
2025-05-18 14:22:50 +0100

    Add functionality to like and remove like on comments, tweets. Test comments.

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -55,11 +55,96 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   //TODO: toggle like on comment
+
+  const userId = req.user?._id;
+
+  if(!commentId || !isValidObjectId(commentId)){
+    throw new ApiError(400, "commentId is not defined or not valid")
+  }
+
+  if(!userId || !isValidObjectId(userId)){
+    throw new ApiError(400, "userId is not defined or not valid")
+  }
+
+  let message = existingLike ? "Comment unliked successfully" : "Comment liked successfully";
+
+  try  {
+
+    const existingLike = await Like.findOne({
+      comment: commentId,
+      likedBy: userId,
+    });
+
+    if(existingLike){
+      await existingLike.deleteOne();
+    } else {
+      await Like.create({
+        comment: commentId,
+        likedBy: userId,
+      });
+    }
+
+    const message = existingLike
+      ? "Comment unliked successfully"
+      : "Comment liked successfully";
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, message));
+
+  } catch (error) {
+
+    console.error(error, error.message);
+    throw new ApiError(500, "Error toggling comment like", {
+      error: error.message
+    })
+  }
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
   //TODO: toggle like on tweet
+  const userId = req.user?._id;
+
+  if(!tweetId || !isValidObjectId(tweetId)){
+    throw new ApiError(400, "tweetId is not defined or not valid")
+  }
+
+  if(!userId || !isValidObjectId(userId)){
+    throw new ApiError(400, "userId is not defined or not valid")
+  }
+
+  try {
+
+    const existingLike = await Like.findOne({
+      tweet: tweetId,
+      likedBy: userId,
+    });
+
+    if(existingLike){
+      await existingLike.deleteOne();
+    } else {
+      await Like.create({
+        tweet: tweetId,
+        likedBy: userId,
+      })
+    }
+
+    const message = existingLike
+      ? "Tweet unliked successfully"
+      : "Tweet liked successfully";
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, message));
+
+    } catch (error) {
+
+    console.error(error, error.message);
+    throw new ApiError(500, "Error toggling tweet like", {
+      error: error.message
+    })
+  }
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleVideoLike, toggleCommentLike, toggleTweetLike } from "../controllers/like.controller.js";
 
 import { verifyJWT } from "../middleware/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/toggleVideoLike/:videoId").post(verifyJWT, toggleVideoLike);
+router.route("/toggleCommentLike/:commentId").post(verifyJWT, toggleCommentLike);
+router.route("/toggleTweetLike/:tweetId").post(verifyJWT, toggleTweetLike);
 
 export default router;
